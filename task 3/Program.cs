193eadb78ab207005a6b3a93a10d291792a81413
unknown <Classtime@PC_6_213_13.aztu.edu.az>
2026-03-06 13:03:44 +0400

    Task 3 nihad

--- task 3/Program.cs
+++ task 3/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("hello world");
+﻿   Console.Write("Eded daxil edin: ");
+        int eded = Convert.ToInt32(Console.ReadLine());
+
+        if (eded > 0)
+        {
+            Console.WriteLine("Eded musbetdir");
+        }
+        else if (eded < 0)
+        {
+            Console.WriteLine("Eded menfidir");
+        }
+        else
+        {
+            Console.WriteLine("Eded sifirdir");
+        }
